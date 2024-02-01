74212b1f089c8f40054c12eb79bd81b8891ff804
DashaAndreeva <143691908+DashaAndreeva@users.noreply.github.com>
2024-02-01 17:52:44 +0200

    continue

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,30 +1,57 @@
 'use strict';
 
 import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const inputElem = document.querySelector('#datetime-picker');
 const buttonElem = document.querySelector('[data-start]');
+const daysElem = document.querySelector('[data-days]');
+const hoursElem = document.querySelector('[data-hours]');
+const minutesElem = document.querySelector('[data-minute]');
+const secondsElem = document.querySelector('[data-seconds]');
 const timerElement = document.querySelector('.timer');
+
 let userSelectedDate;
 
-flatpickr(inputElem, {
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    if (userSelectedDate < Date.now) {
+      iziToast.show({
+        title: 'Please',
+        message: 'choose a date in the future',
+        color: 'red',
+        position: 'topRight',
+      });
+      buttonElem.disabled = true;
+    } else {
+      buttonElem.disabled = false;
+    }
   },
-});
+};
 
-function onDateInput() {
-  const init = Date.now;
-  const choiceDate = inputElem.textContent;
-
-  if (choiceDate > init) {
-    console.log('hello');
-  }
+function padTime(value) {
+  return String(value).padStart(2, '0');
 }
 
-onDateInput();
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+flatpickr(inputElem, options);
