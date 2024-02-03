efba014bb2251d64d50d3a8c23313604e2257939
DashaAndreeva <143691908+DashaAndreeva@users.noreply.github.com>
2024-02-03 17:18:19 +0200

    ex.2

--- src/js/02-snackbar.js
+++ src/js/02-snackbar.js
@@ -5,8 +5,44 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const formElem = document.querySelector('.form');
 const inputElem = formElem.querySelector('input[name="delay"]');
-const fulfilledRadio = formElem.querySelector('input[value="fulfilled"]');
-const rejectedRadio = formElem.querySelector('[value="rejected"]');
-const buttonElem = formElem.querySelector('button');
+const stateRadios = formElem.querySelectorAll('input[name="state"]');
+let promiseStatus = '';
 
-console.log(rejectedRadio);
+function createPromise(e) {
+  e.preventDefault();
+
+  const delay = inputElem.value;
+
+  for (let i = 0; i < stateRadios.length; i++) {
+    if (stateRadios[i].checked) {
+      promiseStatus = stateRadios[i].value;
+      break;
+    }
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    if (promiseStatus === 'fulfilled') {
+      setTimeout(() => resolve(delay), delay);
+    } else if (promiseStatus === 'rejected') {
+      setTimeout(() => reject(delay), delay);
+    }
+  });
+
+  promise
+    .then(delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        color: 'green',
+      });
+    })
+    .catch(delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        color: 'red',
+      });
+    });
+}
+
+formElem.addEventListener('submit', createPromise);
