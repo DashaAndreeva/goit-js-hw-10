0dad510c20d35d4ad53c24e82514caf0ff9fcd81
DashaAndreeva <143691908+DashaAndreeva@users.noreply.github.com>
2024-02-04 13:44:23 +0200

    upd task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -5,42 +5,32 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const formElem = document.querySelector('.form');
 const inputElem = formElem.querySelector('input[name="delay"]');
-const stateRadios = formElem.querySelectorAll('input[name="state"]');
-let promiseStatus = '';
 
 function createPromise(e) {
   e.preventDefault();
 
   const delay = inputElem.value;
-
-  for (let i = 0; i < stateRadios.length; i++) {
-    if (stateRadios[i].checked) {
-      promiseStatus = stateRadios[i].value;
-      break;
-    }
-  }
+  const promiseStatus = formElem.elements.state.value;
 
   const promise = new Promise((resolve, reject) => {
     if (promiseStatus === 'fulfilled') {
       setTimeout(() => resolve(delay), delay);
-    } else if (promiseStatus === 'rejected') {
+    } else {
       setTimeout(() => reject(delay), delay);
     }
   });
 
   promise
     .then(delay => {
-      iziToast.show({
+      iziToast.success({
         message: `✅ Fulfilled promise in ${delay}ms`,
         position: 'topRight',
-        color: 'green',
       });
     })
     .catch(delay => {
-      iziToast.show({
+      iziToast.error({
         message: `❌ Rejected promise in ${delay}ms`,
         position: 'topRight',
-        color: 'red',
       });
     });
 }
